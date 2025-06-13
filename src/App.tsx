e8c418fe05e0cde807ff8bfc7a14b0590e595caf
caiopsilvachess <caiopsilva.dev@gmail.com>
2025-06-13 19:39:42 -0300

    altera layout dos botões de controle dos exercicios

--- src/App.tsx
+++ src/App.tsx
@@ -401,14 +401,23 @@ const ChessboardWithDnd = () => {
           {exercicios[exercicioAtual].white}
         </div>
         <div className="controls">
-          <button onClick={exercicioAnterior} disabled={exercicioAtual === 0}>
-            Anterior
+          <button 
+            onClick={exercicioAnterior} 
+            disabled={exercicioAtual === 0}
+            className="control-button"
+          >
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15 18L9 12L15 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
           </button>
           <button
             onClick={proximoExercicio}
             disabled={exercicioAtual === exercicios.length - 1}
+            className="control-button"
           >
-            Próximo
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M9 6L15 12L9 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
           </button>
         </div>
         <div className="info">
